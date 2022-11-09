62a942b92ff7b588d23ddf63e3ecd248d15e18d9
ChesterD02 <chesterd02@gmail.com>
2022-11-09 15:42:30 -0700

    simple query and mutation

---
+++ functions/edit-profile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports.hello = async (event) => {
+  // Do whatever you want here, but this must return an Object of type MyProfile
+
+  
+
+  const response = {
+    id: "id",
+    name: "name",
+    screenName: "ScreenName",
+    imageUrl: "ImageUrl",
+    backgroundImageUrl: "BackgroundImage URL",
+    createdAt: "12345",
+    activeStacks:
+  }
+
+  return event;
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+};
